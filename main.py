1385b818b73963de971aae9ce8c98b5f66e8691d
samet.alabey <samet.alabey@caretta.net>
2020-10-29 20:03:22 +0300

    code review

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-height = float(input("WRITE YOUR HEIGHT"))
-weight = float(input("WRITE YOUR WEIGHT"))
-b  = pow(height/100, 2)
-a = 0
-BMI = weight / b
-ideal_weight = b * a
+def calculateBmi(weight, height):
+    return weight / pow(height/100, 2)
 
-if BMI < 18.49:
+def calculateIdealWeight(height, weight_limit):
+    return pow(height/100, 2) * weight_limit
 
-    a = 18.49
+bmi_lower_bound = 18.49
+bmi_middle_bound = 24.99
+bmi_higher_bound = 29.99
 
-    ideal_weight = b * a
+weight_limit = 0
+ideal_weight = 0
 
+height = float(input("WRITE YOUR HEIGHT(cm): "))
+weight = float(input("WRITE YOUR WEIGHT(kg): "))
+
+BMI = calculateBmi(weight, height)
+
+if BMI < bmi_lower_bound:
+    ideal_weight = calculateIdealWeight(height, bmi_lower_bound)
     print("You're below of your ideal weight you should get ", round(abs(weight - ideal_weight), 2), "kilos and your BMI is ", round(BMI, 2))
 
-elif BMI >= 18.4 and BMI < 24.99:
-
+elif bmi_lower_bound <= BMI < bmi_middle_bound:
     print("Your BMI is normal and equal to", round(BMI, 2))
 
-elif BMI >= 24.99 and BMI < 29.99:
-
-    a = 24.99
-
-    ideal_weight = b * a
-
+elif bmi_middle_bound <= BMI < bmi_higher_bound:
+    ideal_weight = calculateIdealWeight(height, bmi_middle_bound)
     print("You're above of your ideal weight you should give", round(abs(weight - ideal_weight), 2), "kilos"
-                                                                                                     "and your BMI is", round(BMI, 2))
+                                                                                                     " and your BMI is", round(BMI, 2))
 else:
-    print("DANGER")
+    print('DANGER')
